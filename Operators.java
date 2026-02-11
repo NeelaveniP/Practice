93900777c745e1c82d747d49d5b15e0160e86331
NeelaveniP <neelavenieceneelaveni@gmail.com>
2026-02-11 12:28:18 +0530

    Operators

---
+++ Operators.java
@@ -0,0 +1,29 @@
+public class Operators{
+public static void main (String[]args)
+{
+int p=8;
+int n=14;
+int s=21;
+int m=7;
+
+int a=p*m+n;
+int b=s/m+p;
+int c=s-m*p;
+int d=(p+m)+s;
+
+System.out.println("final output 1 ="+a);
+System.out.println("final output 2 ="+b);
+System.out.println("final output 3 ="+c);
+System.out.println("final output 4 ="+d);
+}
+}
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
