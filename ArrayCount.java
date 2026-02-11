b54fd94a05b94072c913eb7d43483b439cf81a66
NeelaveniP <neelavenieceneelaveni@gmail.com>
2026-02-11 12:21:29 +0530

    ArrayCount

---
+++ ArrayCount.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ArrayaCount{
+	public static void main(String[]args){
+		int a[] ={5,10,15,20,25};
+		int count = 0;
+		for(int i =0 ;i<a.length;i++){
+		count++;
+		}
+		System.out.println(count);
+	}
+}
+		
