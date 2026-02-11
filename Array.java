9c2a75b028130a8ea9d82face8d0d77bda17cab7
NeelaveniP <neelavenieceneelaveni@gmail.com>
2026-02-11 12:23:07 +0530

    Array

---
+++ Array.java
@@ -0,0 +1,12 @@
+public class Array{
+public static void main (String[]args){
+	int [][]a ={
+		{0,1},{1,2}
+	};
+	for (int i=0;i<a.length;i++){
+		for(int j=0;j<a[i].length;j++){
+		System.out.println(a[i][j]);
+	}
+}
+}
+}
